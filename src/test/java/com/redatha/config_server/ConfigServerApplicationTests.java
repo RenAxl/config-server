d95cc60742cdb55d38153fc60bbf3afbbcdd162e
Renan Duarte <renandg.axl@gmail.com>
2024-10-28 19:47:31 -0300

    Creating and configuring the config-server microservice project

---
+++ src/test/java/com/redatha/config_server/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.redatha.config_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
